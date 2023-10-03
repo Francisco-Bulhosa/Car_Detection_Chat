2bf9cc599bbf7d1c84bab830546977d4578fe717
FBF <francisco.bulhosa@gmail.com>
2023-10-03 11:36:26 +0100

    v.0.1.1

--- ViT_test.py
+++ ViT_test.py
@@ -1,27 +1,40 @@
+import tensorflow as tf
 from transformers import AutoFeatureExtractor, AutoModelForImageClassification
+from datasets import load_dataset
+import numpy as np
 
+# Load your data
+data_dir = 'path/to/your/images'
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(224, 224),
+    batch_size=32
+)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(224, 224),
+    batch_size=32
+)
+
+# Load the pre-trained ViT model and feature extractor
 extractor = AutoFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
 model = AutoModelForImageClassification.from_pretrained("google/vit-base-patch16-224")
 
+# Prepare your data
+def prepare_data(images, labels):
+    inputs = extractor(images.numpy(), return_tensors="tf")
+    return inputs['pixel_values'], labels
 
+train_ds = train_ds.map(lambda x, y: tf.py_function(prepare_data, [x, y], [tf.float32, tf.int32]))
+val_ds = val_ds.map(lambda x, y: tf.py_function(prepare_data, [x, y], [tf.float32, tf.int32]))
 
-base_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3),
-                                               include_top=False,
-                                               weights='imagenet')
-base_model.trainable = False
-
-model = tf.keras.Sequential([
-    base_model,
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(1024, activation='relu'),
-    tf.keras.layers.Dense(number_of_classes, activation='softmax')
-])
-
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-model.fit(train_dataset, epochs=5, validation_data=val_dataset)
-
-predictions = model.predict(normalized_input)
-# Decode predictions to get car make, model, and year
+# Fine-tune the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_ds, validation_data=val_ds, epochs=5)
